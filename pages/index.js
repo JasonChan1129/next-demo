571f740da72a3d66dc00a26eb176687a84610ad1
Jason Chan <jasonchan19981017@hotmail.com>
2022-08-20 21:20:40 +0800

    feat: 🎸 add setting page and a link to setting

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
 	return (
@@ -12,6 +12,7 @@ export default function Home() {
 			</Head>
 
 			<h1>This is the index page</h1>
+			<Link href="/setting">Go to setting</Link>
 		</div>
 	);
 }
---
+++ pages/setting.js
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<div className={styles.container}>
+			<h1>This is the setting page</h1>
+		</div>
+	);
+}
